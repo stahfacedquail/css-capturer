f59978d41d5d16be9db1a4121e01e8f28f55f505
tami <stahafacedquail@gmail.com>
2015-01-24 16:27:22 +0200

    Initial commit

---
+++ app/scripts/main.js
@@ -0,0 +1,18 @@
+/*global CSSCapturer, $*/
+
+
+window.CSSCapturer = {
+    Models: {},
+    Collections: {},
+    Views: {},
+    Routers: {},
+    init: function () {
+        'use strict';
+        console.log('Hello from Backbone!');
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    CSSCapturer.init();
+});
